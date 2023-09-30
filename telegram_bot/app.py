eaa29b5c29a714f5c024e4d883e8db9fcaa47f12
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-09-30 21:54:29 +0300

    create app

--- telegram_bot/app.py
+++ telegram_bot/app.py
@@ -1,7 +1,32 @@
 import os
 from dotenv import load_dotenv
 
+import logging
+from telegram import Update 
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
 load_dotenv()
 
+
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-print('Hi')
+if not TELEGRAM_BOT_TOKEN:
+    exit("Specify TELEGRAM_BOT_TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I can talk!")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
