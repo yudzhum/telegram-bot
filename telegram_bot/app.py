042856185181db8e3e54372daa05a74e37e14d80
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-10-03 22:52:17 +0300

    feat: constant messages start and help

--- telegram_bot/app.py
+++ telegram_bot/app.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import logging
 from telegram import Update 
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram_bot import message_texts
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,11 +18,21 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 if not TELEGRAM_BOT_TOKEN:
-    exit("Specify TELEGRAM_BOT_TOKEN")
+    exit("Specify TELEGRAM_BOT_TOKEN env variable")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I can talk!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=message_texts.GREETINGS
+    )
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=message_texts.HELP
+    )
 
 
 if __name__ == '__main__':
@@ -28,5 +40,8 @@ if __name__ == '__main__':
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help)
+    application.add_handler(help_handler)
     
     application.run_polling()
---
+++ telegram_bot/message_texts.py
@@ -0,0 +1,19 @@
+GREETINGS = """
+HI! This is bot for book club.
+
+
+Bot commands:
+
+/start - greeting message.
+/help - info.
+/allbooks - all books from the list.
+/already - book that was read.
+/now - current book.
+/vote - voting for the next book.
+
+"""
+
+
+HELP = """
+help info
+"""
